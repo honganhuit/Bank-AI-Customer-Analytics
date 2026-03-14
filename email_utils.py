f6defaa1e69f275c04599d15a1a339c549905e64
project1 <na02446688@gmail.com>
2026-03-14 16:40:23 +0700

    fix emailutil for streamlit deploy

--- email_utils.py
+++ email_utils.py
@@ -9,12 +9,12 @@ load_dotenv()
 # Lấy email/password
 try:
     # Streamlit Cloud
-    GMAIL_USER = st.secrets["email"]["address"]
-    GMAIL_PASS = st.secrets["email"]["password"]
+    EMAIL_ADDRESS = st.secrets["email"]["address"]
+    EMAIL_PASSWORD = st.secrets["email"]["password"]
 except Exception:
     # Local
-    GMAIL_USER = os.getenv("EMAIL_ADDRESS")
-    GMAIL_PASS = os.getenv("EMAIL_PASSWORD")
+    EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 
 def send_email(to, subject, content):
@@ -22,15 +22,15 @@ def send_email(to, subject, content):
     msg = MIMEText(content)
 
     msg["Subject"] = subject
-    msg["From"] = GMAIL_USER
+    msg["From"] = EMAIL_ADDRESS
     msg["To"] = to
 
     try:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
 
-            server.login(GMAIL_USER, GMAIL_PASS)
+            server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
-            server.sendmail(GMAIL_USER, to, msg.as_string())
+            server.sendmail(EMAIL_ADDRESS, to, msg.as_string())
 
         print("Email sent successfully")
 
